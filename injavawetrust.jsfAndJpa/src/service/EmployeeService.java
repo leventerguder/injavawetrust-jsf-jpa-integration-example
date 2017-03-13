3ea50941127da49f3a958bb3f662017acabc9bd7
leventerguder <erguder.levent@gmail.com>
2017-03-13 20:28:01 +0300

    review

--- injavawetrust.jsfAndJpa/src/service/EmployeeService.java
+++ injavawetrust.jsfAndJpa/src/service/EmployeeService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import model.Employee;
 
 public interface EmployeeService {
-	
+
 	public Employee insertEmployee(Employee employee);
 
 	public Employee findEmployee(int id);
